5fb7a796bfd86183bb7cd10d3e9d9b392a207640
Christian Rodriguez <crodriguez@lyft.com>
2018-12-09 14:06:53 -0800

    basic endpoint for fetching available slots

--- app/app.py
+++ app/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, redirect, render_template, request, g
+from datetime import datetime, timedelta, date, time
+from flask import Flask, redirect, render_template, request, g, jsonify
 from app.services.google_calendar import GoogleCalendarService
 from app.database import get_db
+from pytz import timezone, utc
+from collections import defaultdict
 
 app = Flask(__name__)
 gcal = GoogleCalendarService()
+
+TZ = timezone('America/Los_Angeles')
+WEEK_START_DAY = 0
+WEEK_END_DAY = 4
+DAY_START_HOUR = time(hour=8, tzinfo=TZ)
+DAY_END_HOUR = time(hour=17, tzinfo=TZ)
 
 
 def init_db():
@@ -38,3 +47,75 @@ def handle_google_auth():
     code = auth_data['code']
     gcal.exchange_url_for_token(code)
     return 'We did it'
+
+
+@app.route('/available_slots')
+def available_slots():
+    ret = defaultdict(list)
+
+    # Validate input
+    start_ms = int(request.args.get('start_ms', 0))
+    end_ms = int(request.args.get('end_ms', 0))
+    if not start_ms or not end_ms:
+        return '', 400
+
+    start_dt = datetime.utcfromtimestamp(start_ms).date()
+    end_dt = datetime.utcfromtimestamp(end_ms).date()
+
+    # Get busy slots for all days in time range
+    busy_slots = gcal.get_busy_slots_for_range(start_ms, end_ms)
+
+    # We will return available slots per day
+    current_dt = start_dt
+    while current_dt <= end_dt:
+        # Skip days that are not in office hours
+        weekday = current_dt.weekday()
+        if weekday < WEEK_START_DAY or weekday > WEEK_END_DAY:
+            current_dt += timedelta(days=1)
+            continue
+
+        # Get start and end time for appointments this day
+        start_time = datetime.combine(current_dt, DAY_START_HOUR)
+        end_time = datetime.combine(current_dt, DAY_END_HOUR)
+
+        # Go through busy slots per day and find buckets of time within our range
+        # where there are no busy slots. This makes the assumption that busy slots
+        # are sorted by (start, end)
+        busy_slots_for_day = busy_slots.get(current_dt)
+        if busy_slots_for_day is not None:
+            for busy_slot in busy_slots_for_day:
+                # We've update start_time past our office hours. Done for this date
+                if start_time > end_time:
+                    break
+                busy_start, busy_end = busy_slot['start'], busy_slot['end']
+
+                # Busy slot before office hours start
+                if busy_end < start_time:
+                    continue
+
+                # Busy slot overlaps with current time. Update current start
+                elif busy_start <= start_time and busy_end > start_time:
+                    start_time = busy_end
+                    continue
+
+                # Busy slot start after current time
+                # Add open slot to output and update current time
+                elif busy_start > start_time:
+                    ret[current_dt.isoformat()].append({
+                        'start': start_time.isoformat(),
+                        'end': busy_start.isoformat(),
+                    })
+                    start_time = busy_end
+                    continue
+
+        # Add last open slot to output if it exists
+        if start_time < end_time:
+            ret[current_dt.isoformat()].append({
+                'start': start_time.isoformat(),
+                'end': end_time.isoformat(),
+            })
+
+        # Process next date
+        current_dt += timedelta(days=1)
+
+    return jsonify(ret)
--- app/services/google_calendar.py
+++ app/services/google_calendar.py
@@ -1,14 +1,20 @@
-from typing import Tuple
+from datetime import datetime, date
+from typing import Tuple, Dict, Any
 import json
+from pytz import timezone, utc
 
-import google.oauth2.credentials
+from google.oauth2.credentials import Credentials
 import google_auth_oauthlib.flow
+import googleapiclient.discovery
 from app.database import query_db
 
+# TODO: We'll make this the timezone of the calendar
+TZ = timezone('America/Los_Angeles')
 
 class GoogleCalendarService():
     def __init__(self):
         scopes = [
+            'https://www.googleapis.com/auth/calendar',
             'https://www.googleapis.com/auth/calendar.events',
             'https://www.googleapis.com/auth/plus.me',
             'https://www.googleapis.com/auth/userinfo.email',
@@ -38,3 +44,57 @@ class GoogleCalendarService():
         ]
         query_db(query, values)
         return credentials
+
+    def get_user_creds(self, user_id: int):
+        query = 'select * from google_credentials where id=?'
+        credentials = query_db(query, [user_id], one=True)
+        return credentials
+
+    def get_busy_slots_for_range(self, start_ms, end_ms):
+        ret = {}
+        # Get credentials for query
+        db_creds = self.get_user_creds(3)
+        credentials = Credentials(
+            token=db_creds['token'],
+            refresh_token=db_creds['refresh_token'],
+            token_uri=db_creds['token_uri'],
+            client_id=db_creds['client_id'],
+            client_secret=db_creds['client_secret'],
+            scopes=json.loads(db_creds['scopes']),
+        )
+
+        # Query params
+        start = datetime.utcfromtimestamp(start_ms).isoformat() + 'Z'
+        end = datetime.utcfromtimestamp(end_ms).isoformat() + 'Z'
+        service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
+
+        body = {
+            'timeMin': start,
+            'timeMax': end,
+            'items': [{'id': 'primary'}],
+        }
+        freebusy = service.freebusy().query(body=body).execute()
+        print(freebusy)
+        busy_slots = freebusy['calendars']['primary']['busy']
+
+        # parse busy slots into dt objects
+        for slot in busy_slots:
+            slot_start = utc.localize(datetime.fromisoformat(slot['start'][:-1])).astimezone(TZ)
+            slot_end = utc.localize(datetime.fromisoformat(slot['end'][:-1])).astimezone(TZ)
+            slot_date = slot_start.date()
+
+            # TODO: Make sure these are sorted
+            data = {
+                'start': slot_start,
+                'end': slot_end,
+            }
+
+            if slot_date in ret:
+                ret[slot_date].append(data)
+            else:
+                ret[slot_date] = [data]
+
+        return ret
+
+    def is_time_in_busy_slots(self, start_dt, end_dt) -> bool:
+        pass
